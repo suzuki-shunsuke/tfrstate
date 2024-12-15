ed34e44adf57e210a4fb4cef5189048bde558cf5
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-12-15 13:09:24 +0900

    feat: support finding directories without plan file and output names (#42)
    * feat: support finding directories without plan file and output names
    
    * test: fix test data

--- pkg/controller/find/caller.go
+++ pkg/controller/find/caller.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 )
 
-func findCaller(dirs map[string]*Dir, changedOutputs []string, changed map[string]map[string]map[string]struct{}) { //nolint:gocognit
+func findCaller(dirs map[string]*Dir, changedOutputs []string, changed map[string]map[string]map[string]struct{}) {
 	// Find files referring terraform_remote_state
 	for _, dir := range dirs {
 		for _, file := range dir.Files {
@@ -12,26 +12,43 @@ func findCaller(dirs map[string]*Dir, changedOutputs []string, changed map[strin
 				continue
 			}
 			for _, state := range dir.States {
-				if !strings.Contains(file.Content, "data.terraform_remote_state."+state.Name+".outputs.") {
-					continue
-				}
-				for _, outputName := range changedOutputs {
-					if !strings.Contains(file.Content, "data.terraform_remote_state."+state.Name+".outputs."+outputName) {
-						continue
-					}
-					m, ok := changed[dir.Path]
-					if !ok {
-						m = map[string]map[string]struct{}{}
-					}
-					m2, ok := m[file.Path]
-					if !ok {
-						m2 = map[string]struct{}{}
-					}
-					m2[outputName] = struct{}{}
-					m[file.Path] = m2
-					changed[dir.Path] = m
-				}
+				findCallerCore(dir, file, state, changedOutputs, changed)
 			}
 		}
+	}
+}
+
+func findCallerCore(dir *Dir, file *File, state *RemoteState, changedOutputs []string, changed map[string]map[string]map[string]struct{}) {
+	if !strings.Contains(file.Content, "data.terraform_remote_state."+state.Name+".outputs.") {
+		return
+	}
+	if len(changedOutputs) == 0 {
+		m, ok := changed[dir.Path]
+		if !ok {
+			m = map[string]map[string]struct{}{}
+		}
+		m2, ok := m[file.Path]
+		if !ok {
+			m2 = map[string]struct{}{}
+		}
+		m[file.Path] = m2
+		changed[dir.Path] = m
+		return
+	}
+	for _, outputName := range changedOutputs {
+		if !strings.Contains(file.Content, "data.terraform_remote_state."+state.Name+".outputs."+outputName) {
+			continue
+		}
+		m, ok := changed[dir.Path]
+		if !ok {
+			m = map[string]map[string]struct{}{}
+		}
+		m2, ok := m[file.Path]
+		if !ok {
+			m2 = map[string]struct{}{}
+		}
+		m2[outputName] = struct{}{}
+		m[file.Path] = m2
+		changed[dir.Path] = m
 	}
 }
